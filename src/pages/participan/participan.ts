e5b5270fd62eeb3ea24730fbb05ec72e9756d0e9
Ellery Valest <ellerysammy@gmail.com>
2018-07-25 11:21:58 -0500

    Redesign existing pages

--- src/pages/participan/participan.ts
+++ src/pages/participan/participan.ts
@@ -14,20 +14,23 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ParticipanPage {
   participantes: any = [];
+  participaSelected: string = "";
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.participantes = [
       {
         name: "Autoridades Ambientales",
+        value: "AMB",
         childs: [
           {name: "Cardique", logo:"cardique.jpg"},
           {name: "Corporación Autónoma Regional del Sur de Bolívar", logo:"csb.jpg"},
           {name: "Establecimiento Público Ambiental", logo:"epa.jpg"},
           {name: "Cormagdalena", logo:"cormagdalena.jpg"},
-          {name: "Parques Nacionales naturales", logo:"pnn.jpg"}
+          {name: "Parques Nacionales Naturales", logo:"pnn.jpg"}
         ]
       },
       {
         name: "POTS Modernos",
+        value: "PMO",
         childs: [
           {name: "Inypsa", logo:"inypsa.png"}
         ]
